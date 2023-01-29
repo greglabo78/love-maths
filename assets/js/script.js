623a8d84e11389a88637f109163d13e939348454
greglabo78 <gregoryobigodwin@yahoo.com>
2023-01-29 15:19:36 +0000

    Set up struture of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function runGames() {
+
+}
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+    
+}
