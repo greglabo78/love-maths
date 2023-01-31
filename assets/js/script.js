00941566b8f1f8f3dc8f8bd826be03ea79717a65
greglabo78 <gregoryobigodwin@yahoo.com>
2023-01-31 11:49:10 +0000

    reviewed and corrected the division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     }else if (operator === "/") {
-        return [Math.round(operand1 / operand2), "division"];
+        return [operand1 / operand2, "division"];
     }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
@@ -142,9 +142,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2
 
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
     document.getElementById('operator').textContent = "/";
 
 }
